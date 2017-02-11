2573434d2231514b102f5dd1fae38a340555777d
mnelli19 <nllmra19@gmail.com>
2017-02-11 16:27:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/upload', upload.single('file'), (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('App listening on port 3000!')
 });
 
