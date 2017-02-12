284072ff24e77f5682005294c49a90e6d8c76ff8
mnelli19 <nllmra19@gmail.com>
2017-02-12 19:21:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,22 +7,6 @@ const app = express();
 const multer = require('multer');
 
 
-//bluemix file storage modificato
-var pkgcloud = require('pkgcloud');
-
-var config = {
-    provider: 'openstack',
-    useServiceCatalog: true,
-    useInternal: false,
-    keystoneAuthVersion: 'v3',
-    authUrl: 'https://identity.open.softlayer.com',
-    tenantId: '80e33159813f48739f09570464e566c4',    //projectId from credentials
-    domainId: '5c97167852de417884764ac2ae2c25ca',
-    username: 'admin_9757dce54df22d39aebe60045e8949690d5ad7fe',
-    password: 'p?v.}M2N*1nQ6YQ(',
-    region: 'dallas'   //dallas or london region
-};
-// end bluemix ///
 
 let storage = multer.memoryStorage();
 
@@ -35,57 +19,13 @@ app.use(express.static(__dirname + '/public'));
 
 app.post('/upload', upload.single('file'), (req, res) => {
  console.log("**** chiamata POST >>>> ");
-    let file = req.file; //modificato
+    let file = req.file;
     let body = req.body;
-
-    //modifica bluemix ///
-    console.log("**** file req >>>> "+req.file);
-    console.log("**** identifier >>>> "+req.identifier);
-    console.log("**** file >>>> "+req.filename);
-    //console.log("**** body >>>> "+body);
-    console.log("**** req filename >>>> "+req.filename);
     
-    var storageClient = pkgcloud.storage.createClient(config);
-
-    storageClient.auth(function(err) {
-        if (err) {
-            console.error(err);
-        }
-        else {
-            console.log(storageClient._identity);
-        }
-        
-    });
-    
-    var myFile = fs.createReadStream(req.file);
-
-        var upload = storageClient.upload({
-            container: "FlowJsNode",
-            remote: file
-        });
-
-        upload.on('error', function(err) {
-            console.log("**** ERROR >>>> ");
-            console.error(err);
-            res.send();
-        });
-
-        upload.on('success', function(file) {
-            console.log("**** SUCCESS >>>> ");
-            console.log(file.toJSON());
-            res.send();
-        });
-
-        myFile.pipe(upload);
-        
-         
-// end bluemix ///
-   
- /* modificato
     flow.upload(file, body, () => {
         res.send();
     });
-    */
+   
          
 });
 
