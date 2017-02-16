c8fb1e94af24891322e0b4daaa2a261e3be68f63
Alessandro Mancini <alexemancio1985@gmail.com>
2017-02-16 10:07:19 +0100

    Update server.js
    Commit

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 
-
-
 let storage = multer.memoryStorage();
 
 const upload = multer({
@@ -21,26 +19,28 @@ app.post('/upload', upload.single('file'), (req, res) => {
  console.log("**** chiamata POST >>>> ");
     let file = req.file;
     let body = req.body;
-    
-    flow.upload(file, body, () => {
+
+    flow.upload(file, body, (err, result) => {
         res.send();
     });
-   
-         
+
+
 });
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('App listening on port 3000!')
 });
 
-app.get('/download/:identifier/:filename', function(req, res) {
+app.get('/download/:identifier/:filename/:number', function(req, res) {
     console.log("**** chiamata GET >>>> ");
     let identifier = req.params.identifier;
     let filename = req.params.filename;
-    flow.download(identifier, filename, (stream) => {
+    let number = req.params.number;
+    flow.download(identifier, filename, number, (err, result) => {
 
         res.setHeader('Content-disposition', 'attachment; filename=' + filename);
         res.setHeader('Content-type', 'application/octet-stream');
-        stream.pipe(res);
+        result.pipe(res);
+
     });
 });
