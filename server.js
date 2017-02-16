71475afa22c01d95eb7675f639eaf67264c8c463
Alessandro Mancini <alexemancio1985@gmail.com>
2017-02-16 10:09:46 +0100

    Update server.js
    commit

--- server.js
+++ server.js
@@ -1,118 +1,46 @@
 'use strict';
 
-const fs = require('fs-extra');
-fs.readdir = require('fs').readdir;
-fs.open = require('fs').open;
-fs.write = require('fs').write;
-const Readable = require('stream').Readable;
-const util = require('util');
-const PassThrough = require('stream').PassThrough;
-const path = require('path');
-var pkgcloud = require('pkgcloud');
+const UPLOADED_DIR = "uploaded/";
 
-let StreamCombiner = function() {
-  this.streams = arguments[0];
+const express = require('express');
+const app = express();
+const multer = require('multer');
 
-  this.on('pipe', function(source) {
-    source.unpipe(this);
-    for(let i in this.streams) {
-      console.log("Sto leggendo il "+i+" stream...");
-      source = source.pipe(this.streams[i]);
-    }
-    this.transformStream = source;
-  });
-};
+let storage = multer.memoryStorage();
 
-util.inherits(StreamCombiner, PassThrough);
+const upload = multer({
+    storage: storage
+});
+const flow = require('./flowjs')(UPLOADED_DIR);
 
-StreamCombiner.prototype.pipe = function(dest, options) {
-  return this.transformStream.pipe(dest, options);
-};
+app.use(express.static(__dirname + '/public'));
 
-var config = {
-    provider: 'openstack',
-    useServiceCatalog: true,
-    useInternal: false,
-    keystoneAuthVersion: 'v3',
-    authUrl: 'https://identity.open.softlayer.com',
-    tenantId: '80e33159813f48739f09570464e566c4', //projectId from credentials
-    domainId: '5c97167852de417884764ac2ae2c25ca',
-    username: 'admin_9757dce54df22d39aebe60045e8949690d5ad7fe',
-    password: 'p?v.}M2N*1nQ6YQ(',
-    region: 'dallas' //dallas or london region
-};
+app.post('/upload', upload.single('file'), (req, res) => {
+ console.log("**** chiamata POST >>>> ");
+    let file = req.file;
+    let body = req.body;
 
-const storageClient = pkgcloud.storage.createClient(config);
-const CONTAINER_NAME = 'my-container';
-
-let upload = (uploadedDir) => {
-
-    const UPLOADED_DIR = uploadedDir
-
-    let uploadChunk = (file, name, callback) => {
-        let stream = new Readable()
-        stream.push(file.buffer);
-        stream.push(null);
+    flow.upload(file, body, (err, result) => {
+        res.send();
+    });
 
 
-        storageClient.createContainer({
-            name: CONTAINER_NAME
+});
 
-        }, function(err, container) {
-            if (err) return callback(err);
+app.listen(process.env.PORT || 3000, () => {
+    console.log('App listening on port 3000!')
+});
 
-            let upload = storageClient.upload({
-                container: container.name,
-                remote: name
-            });
+app.get('/download/:identifier/:filename/:number', function(req, res) {
+    console.log("**** chiamata GET >>>> ");
+    let identifier = req.params.identifier;
+    let filename = req.params.filename;
+    let number = req.params.number;
+    flow.download(identifier, filename, number, (err, result) => {
 
-            upload.on('error', function(err) {
-                console.error(err);
-            });
+        res.setHeader('Content-disposition', 'attachment; filename=' + filename);
+        res.setHeader('Content-type', 'application/octet-stream');
+        result.pipe(res);
 
-            upload.on('success', function(file) {
-                console.log(file.toJSON());
-                callback(null, "OK");
-            });
-
-            stream.pipe(upload);
-
-        });
-
-    }
-
-    return {
-        upload: (file, body, callback) => {
-            let name = body.flowFilename + "." + body.flowChunkNumber;
-            uploadChunk(file, name, callback)
-        },
-        download: (identifier, filename, number, callback) => {
-
-            let merge2 = require('merge2');
-
-
-            let range = Array.from({
-                length: number
-            }, (_, i) => i + 1);
-
-            let streams = range.map((elem) => {
-                let stream = storageClient.download({
-                    container: CONTAINER_NAME,
-                    remote: filename + "." + elem
-                })
-                return stream;
-            });
-
-            let result = merge2(streams)
-
-            result.on('queueDrain', function() {
-              console.log("queueDrain ...");
-              return callback(null, result);
-            })
-
-        }
-    }
-
-}
-
-module.exports = upload;
+    });
+});
