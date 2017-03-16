edb39f0251d30027b1e6bc16c95c7b68967795ac
mnelli19 <nllmra19@gmail.com>
2017-03-16 22:39:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.all('/', function(req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
- });
+});
 
 app.post('/prepare', (req, res) => {
     let body = req.body;
